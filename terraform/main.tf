c0b8211a57c4308f22407535a75a3c91e2400b0b
JFZ <zeiglerit@gmailcom>
2025-09-24 12:19:39 -0500

    updates

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  assume_role {
+    role_arn = var.aws_iam_role_arn
+  }
+}
+
+module "r_lambda" {
+  source         = "../aws/multilang_lambda"
+  function_name  = var.function_name
+  s3_bucket      = var.s3_bucket
+  s3_key         = var.s3_key
+  runtime        = "provided.al2"
+  handler        = "bootstrap"
+}
---
+++ terraform/providers/aws.tf
@@ -0,0 +1,7 @@
+output "lambda_function_name" {
+  value = module.r_lambda.function_name
+}
+
+output "lambda_arn" {
+  value = module.r_lambda.lambda_arn
+}
