4968bbc13b173ff6da91c472531feccf231a0f22
JFZ <zeiglerit@gmailcom>
2025-10-05 11:20:34 -0500

    update

--- tools/gcp_list_resources.sh
+++ tools/gcp_list_resources.sh
@@ -1,1 +1,3 @@
-gcloud asset search-all-resources --project=my-project-6440-472920
+gcloud asset search-all-resources \
+  --project=my-project-6440-472920 \
+  --format="table(assetType, name, location)"
